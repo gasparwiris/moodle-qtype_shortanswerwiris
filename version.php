8c7c34f72ac4055863d60c60377d59674b4857a8
Manuel Cagigas <manuel@wiris.com>
2016-11-17 13:31:55 +0100

    Add release to version.php

--- version.php
+++ version.php
@@ -16,9 +16,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016111600;
+$plugin->version = 2016111700;
+$plugin->release = '3.46.0.1053';
 $plugin->requires = 2011060313;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2016111600
+     'qtype_wq' => 2016111700
 );
